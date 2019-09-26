a4e25c0c937241ba750cc02f56c99931c7b2e72e
Maxime Parisse <maxime@epic.net>
2019-09-26 10:49:25 +0200

    Improve single-view

--- pages/experiment/_id.vue
+++ pages/experiment/_id.vue
@@ -1,7 +1,5 @@
-import 
-
 <template>
-  <div>
+  <div class="inner">
     <div>
       {{ post.attributes.path }}<br>
       {{ post.attributes.date }}<br>
@@ -28,6 +26,9 @@ import
 </template>
 
 <script>
+  import hljs from 'highlight.js/lib/highlight';
+  import javascript from 'highlight.js/lib/languages/javascript';
+
   export default {
 		computed: {
 			post () {
@@ -39,6 +40,11 @@ import
 		},
 
     mounted() {
+      hljs.registerLanguage('javascript', javascript);
+      document.querySelectorAll('code').forEach(c => {
+        hljs.highlightBlock(c);
+      });
+
       this.foo();
     },
 
@@ -61,4 +67,71 @@ import
     width: 100%;  
     max-width: 800px;
   }
+
+  .inner {
+    max-width: 800px;  
+    margin: 80px auto;
+  }
+
+  .hljs {
+    display: block;
+    overflow-x: auto;
+    padding: 0.5em;
+    background: #282a36;
+  }
+
+  .hljs-keyword,
+  .hljs-selector-tag,
+  .hljs-literal,
+  .hljs-section,
+  .hljs-link {
+    color: #8be9fd;
+  }
+
+  .hljs-function .hljs-keyword {
+    color: #ff79c6;
+  }
+
+  .hljs,
+  .hljs-subst {
+    color: #f8f8f2;
+  }
+
+  .hljs-string,
+  .hljs-title,
+  .hljs-name,
+  .hljs-type,
+  .hljs-attribute,
+  .hljs-symbol,
+  .hljs-bullet,
+  .hljs-addition,
+  .hljs-variable,
+  .hljs-template-tag,
+  .hljs-template-variable {
+    color: #f1fa8c;
+  }
+
+  .hljs-comment,
+  .hljs-quote,
+  .hljs-deletion,
+  .hljs-meta {
+    color: #6272a4;
+  }
+
+  .hljs-keyword,
+  .hljs-selector-tag,
+  .hljs-literal,
+  .hljs-title,
+  .hljs-section,
+  .hljs-doctag,
+  .hljs-type,
+  .hljs-name,
+  .hljs-strong {
+    font-weight: bold;
+  }
+
+  .hljs-emphasis {
+    font-style: italic;
+  } 
+
 </style>
