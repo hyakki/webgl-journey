965303c0b1f69040f3a6583e49bf4dfdfed0c635
hyakk1 <hyakk1@hyakk1-mbp.epic-sys.io>
2019-09-27 16:25:28 +0200

    Improve the 06 exp

--- assets/js/experiments/06-normals/index.js
+++ assets/js/experiments/06-normals/index.js
@@ -30,7 +30,7 @@ class Exp {
     this.regl({
 
       vert: `
-        precision mediump float;
+        precision highp float;
         attribute vec3 position;
         attribute vec3 normal;
         uniform mat4 projection;
@@ -43,7 +43,7 @@ class Exp {
       `,
 
       frag: `
-        precision mediump float;
+        precision highp float;
         varying vec3 vNormal;
         void main () {
           gl_FragColor = vec4(vNormal * 0.5 + 0.5, 1.0);
