21968e3bd69e77b65563f6d4f851eba466ab77c1
Maxime Parisse <maximeparisse@gmail.com>
2019-09-27 23:34:27 +0200

    Updates

--- _nuxt/fefd79e497753fca6484.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{153:function(e,t){e.exports={attributes:{path:"/experiment/03-cube",date:"2019-09-08",title:"03-cube",slug:"03-cube",cover:"./../images/covers/03-cube.png"},html:"<h2>Content</h2>\n<h2>Code highlights</h2>\n<h2>Sources</h2>\n"}}}]);
