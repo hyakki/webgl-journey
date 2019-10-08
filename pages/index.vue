415b38b796d2ad547dd7ea0dc45c478f7342c79a
Maxime Parisse <maxime@epic.net>
2019-10-08 18:05:58 +0200

    Improve styles

--- pages/index.vue
+++ pages/index.vue
@@ -26,32 +26,37 @@
   };
 </script>
 
-<style>
+<style lang="scss">
   .posts {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 20px;
     padding: 20px;
     max-width: 900px;
     margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 
   .post {
     text-decoration: none; 
-    display: block;
+    display: flex;
+    width: 100%;
+
+    & + & {
+      margin-top: 20px;  
+    }
   }
 
   .post__picture-outer {
     position: relative;
-
+    width: 35%;
+    flex-shrink: 0;
   }
 
   .post__picture-outer::before {
     content: '';
     display: block;
     width: 100%;
-    padding-top: 100%;
+    padding-top: 70%;
   }
 
   .post__picture {
@@ -65,10 +70,20 @@
   }
 
   .post__title {
-    margin-top: 10px;
-    color: black;
+    color: #000;
     font-size: 1.6rem;
     line-height: 1.2em;
+    flex-grow: 1;
+    padding: 20px;
+    border: 1px solid #efefef;
+    border-left: 0;
+    transition: background-color .2s ease-out, color .2s ease-out;
+
+    .post:hover & { 
+      background-color: #808080;
+      color: #fff;  
+    }
   } 
+
 </style>
 
