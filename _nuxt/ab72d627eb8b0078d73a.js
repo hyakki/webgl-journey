70fbb329d52bb33a41d451df42e97eb1a5bbb519
Maxime Parisse <maxime@epic.net>
2019-10-11 18:05:20 +0200

    Updates

--- _nuxt/ab72d627eb8b0078d73a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[43],{306:function(n,o,t){"use strict";t.r(o),o.default="precision highp float;\n\nattribute vec3 position;\n\nuniform mat4  projection;\nuniform mat4  view;\n\nattribute vec2 uv;\nvarying vec2 vUv;\n\nvoid main () {\n  vUv = uv;\n\n  vec3 p = position;\n\n  gl_Position = projection * view * vec4(p, 1.0);\n}\n"}}]);
